40a1c59b1d4678122bab668de76149ce4e9c550f
Yodaman07 <ayaan.irshad@gmail.com>
2025-09-30 15:09:57 -0700

    maybe now

--- arachne_control.py
+++ arachne_control.py
@@ -46,6 +46,7 @@ class ArachneController:
 
         self.autonomous = False  # teleop mode for default
         self.pause_event = threading.Event()
+        self.data = "NA"
 
         # Initializing all leg ids
         self.all_legs = (2, 3, 4, 5, 1, 0)
@@ -189,7 +190,6 @@ class ArachneController:
 
     # MAIN METHOD
     def start_ps4_ctrl(self, socket):
-        global data
         ###### PS4 Remote Control ######
         # consider using a dedicated library for ps4 controlling with pyPS4Controller
         # pip install pyPS4Controller
@@ -199,8 +199,7 @@ class ArachneController:
         muscle_up = False
         # busy = False
 
-        data = []
-        autonomousThread = threading.Thread(target=self.controller_mixin, args=data)
+        autonomousThread = threading.Thread(target=self.controller_mixin)
         autonomousThread.start()
         self.pause_event.clear()  # by default start thread and then pause it
         cap = cv.VideoCapture(0)
@@ -288,7 +287,7 @@ class ArachneController:
                 elif walking and (abs(left_stick_y_axis) <= deadzone) and (abs(left_stick_x_axis) <= deadzone):
                     walking = False
             else:
-                data = Client.client_tick(cap, socket)
+                self.data = Client.client_tick(cap, socket)
         # Cleanup
 
     # Movement Functions
@@ -392,19 +391,19 @@ class ArachneController:
             # legs_step_relative(turn_legs, 0, 0, 0, 30, False)
             # time.sleep(delay)
 
-    def controller_mixin(self, returned):
+    def controller_mixin(self):
         print("Init Thread")
         # Pt Key:
         # [[x,y],[x,y],[x,y]] Options are: "NA", "[_,_]" <- actual point
         while True:
             self.pause_event.wait()
-            if returned == "NA":  # scanning for an object
+            if self.data == "NA":  # scanning for an object
                 print("TURN")
                 self.crab_walk_turn(10)
             else:
-                print(returned)  # if multiple points try to walk in between them, only sending one for now though
+                print(self.data)  # if multiple points try to walk in between them, only sending one for now though
                 print("CRAB WALK")
-                x, y = returned[0][0], returned[0][1]
+                x, y = self.data[0][0], self.data[0][1]
                 if x < (320-20): # width is 640, # turn right
                     self.crab_walk_turn(10)
                 elif x > (320+20):  # turn left
